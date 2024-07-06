69ed2b7dd63718570fc70669f8e79ebfc2149b10
Landon Trinh <111808279+landont168@users.noreply.github.com>
2024-07-06 11:36:19 -0400

    add: create helper function and unit tests

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+// return total sum of likes in all blogs posts
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes
+  }
+
+  return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }

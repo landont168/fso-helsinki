0ad9a5ae2c7c5ec98047b90e4609f716557ff192
Landon Trinh <111808279+landont168@users.noreply.github.com>
2024-07-27 17:40:27 -0400

    add: create custom hook for fields of form

--- part7/README.md
+++ part7/README.md
@@ -1,1 +1,7 @@
 # Part 7 - React router, custom hooks, styling app with CSS and webpack
+
+`routed-anecdotes`
+
+- 7.1 - add `React Router` to create Menu component
+- 7.2 - implement a view for showing a single anecdote, using `useMatch` hook to extract ID
+- 7.3 - transitions to view after creating new form, using `useNavigate` hook
--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Routes, Route, Link, useMatch, useNavigate } from 'react-router-dom'
+import { useField } from './hooks'
 
 const Menu = ({ anecdotes, addNew }) => {
   const padding = {
@@ -99,21 +100,26 @@ const Footer = () => (
 
 const CreateNew = (props) => {
   const navigate = useNavigate()
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const { reset: contentReset, ...content } = useField('text')
+  const { reset: authorReset, ...author } = useField('text')
+  const { reset: infoReset, ...info } = useField('text')
 
+  // reset form fields
+  const handleReset = () => {
+    contentReset()
+    authorReset()
+    infoReset()
+  }
+
+  // save form fields to state
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     })
-    setContent('')
-    setAuthor('')
-    setInfo('')
     navigate('/')
   }
 
@@ -124,30 +130,19 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name='content'
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name='author'
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name='info'
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
+      <button onClick={handleReset}>reset</button>
     </div>
   )
 }

1311e1050ae1ae153142ef549317cea3889d6f98
Landon Trinh <111808279+landont168@users.noreply.github.com>
2024-12-25 16:55:17 -0500

    add: error safety

--- part9/flight-diary/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary/flight-diary-frontend/src/App.tsx
@@ -1,15 +1,17 @@
 import Diaries from "./components/Diaries";
+import axios from "axios";
 import { useState, useEffect } from "react";
 import { Diary, NewDiary, Weather, Visibility } from "./types";
 import { getAllDiaries, createDiary } from "./services/diariesService";
 
 const App = () => {
   const [diaries, setDiaries] = useState<Diary[]>([]);
+  const [errorMsg, setErrorMsg] = useState("");
 
   // form data fields
   const [date, setDate] = useState("");
-  const [visibility, setVisibility] = useState("");
-  const [weather, setWeather] = useState("");
+  const [visibility, setVisibility] = useState("great");
+  const [weather, setWeather] = useState("sunny");
   const [comment, setComment] = useState("");
 
   const addDiary = (event: React.SyntheticEvent) => {
@@ -21,13 +23,21 @@ const App = () => {
       visibility: visibility as Visibility,
       comment,
     };
-    createDiary(newDiary).then((diary) => {
-      setDiaries(diaries.concat(diary));
-      setDate("");
-      setVisibility("");
-      setWeather("");
-      setComment("");
-    });
+
+    // create diary but catch possible errors
+    createDiary(newDiary)
+      .then((diary) => {
+        setDiaries(diaries.concat(diary));
+        setDate("");
+        setVisibility("");
+        setWeather("");
+        setComment("");
+      })
+      .catch((error) => {
+        if (axios.isAxiosError(error) && error.response) {
+          setErrorMsg(error.response.data);
+        }
+      });
   };
 
   // fetch entries from backend
@@ -39,26 +49,39 @@ const App = () => {
     <div>
       <form onSubmit={addDiary}>
         <h2>Add new entry</h2>
+        <div style={{ color: "red" }}>{errorMsg}</div>
         <div>
           <label>date</label>
           <input
+            type="date"
             value={date}
             onChange={(event) => setDate(event.target.value)}
           />
         </div>
         <div>
           <label>visibility</label>
-          <input
+          <select
             value={visibility}
             onChange={(event) => setVisibility(event.target.value)}
-          />
+          >
+            <option value="great">great</option>
+            <option value="good">good</option>
+            <option value="ok">ok</option>
+            <option value="poor">poor</option>
+          </select>
         </div>
         <div>
           <label>weather</label>
-          <input
+          <select
             value={weather}
             onChange={(event) => setWeather(event.target.value)}
-          />
+          >
+            <option value="sunny">sunny</option>
+            <option value="rainy">rainy</option>
+            <option value="cloudy">cloudy</option>
+            <option value="stormy">stormy</option>
+            <option value="windy">windy</option>
+          </select>
         </div>
         <div>
           <label>comment</label>
